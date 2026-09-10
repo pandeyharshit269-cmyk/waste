624348a5c416740a3479611c5583cd5c871c6a87
pandeyharshit269-cmyk <pandeyharshit269@gmail.com>
2026-09-10 22:33:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TNL_NETWORK")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+adNifJvmcZZlNjA9")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
